b170f6ae8d8ebe0842d7caf9dca88dec3230c74b
pavel.molina <pavel.molina@patronato.unam.mx>
2021-04-13 06:28:56 +0000

    Se agrego el modelo Course

---
+++ odoo_academy/models/course.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    
+    _name = 'academy.course'
+    _description = 'Course Info'
+    
+    name = fields.Char(string='Titulo', required=True)
+    description = fields.Text(string='Descripcion')
+    
+    level = fields.Selection(string='Nivel',
+                            selection=[('beginner','Principiante'),
+                                       ('intermediate','Intermedio'),
+                                       ('Advanced','Avanzado')],
+                            copy=False)
+    
+    active = fields.Boolean(string='Activo', default=True)
