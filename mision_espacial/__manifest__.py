e56c18c6123bc7d482e4452db0a61970fe89c1fd
pavel.molina <pavel.molina@patronato.unam.mx>
2021-04-16 05:51:27 +0000

    [ADD] Modulo Basico

---
+++ mision_espacial/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Misión Espacial',
+    
+    'summary': """Misión Espacial a la Luna""",
+    
+    'description': """
+          Misión Espacial a la Luna que incluirá:
+          - La nave espacial
+          - La tripulación
+    """,
+    
+    'author': 'Pavel',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+       
+        
+    ],
+    
+    'demo':[
+        
+        
+    ],
+    
+}
