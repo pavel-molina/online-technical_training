06e771852a3ac2fd8c1dcf312b46f3101304121f
pavel-molina <pavel.molina@patronato.unam.mx>
2021-04-26 01:44:11 +0000

    [ADD] Website controllers

--- odoo_academy/controllers/academy_controllers.py
+++ odoo_academy/controllers/academy_controllers.py
@@ -7,10 +7,15 @@ class Academy(http.Controller):
     def index(self, **kw):
         return "Hola Mundo"
     
-    @http.route('/academy/courses', auth='public', website=True)
+    @http.route('/academy/courses/', auth='public', website=True)
     def courses(self, **kw):
         courses = http.request.env['academy.course'].search([])
-        return http.request.render('academy.course_website', {
+        return http.request.render('odoo_academy.course_website', {
             'courses': courses,
         })
 
+    @http.route('/academy/<model("academy.session"):session>/', auth='public', website=True)
+    def session(self, session):
+        return http.request.render('odoo_academy.session_website', {
+            'session': session,
+        })
