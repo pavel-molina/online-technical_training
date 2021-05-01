44a6251d40662298bba826b44d44f9d5262deaad
pavel-molina <pavel.molina@patronato.unam.mx>
2021-05-01 23:00:41 +0000

    [ADD] Modelo Libro

---
+++ admin_biblioteca/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Biblioteca',
+    
+    'summary': """Administración de la Biblioteca""",
+    
+    'description': """
+          Procesará a los clientes sacando libros
+          y organizará libros y alquileres
+    """,
+    
+    'author': 'Pavel',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [   
+       
+    ],
+    
+    'demo':[
+        
+        
+    ],
+    
+}
---
+++ admin_biblioteca/models/libro.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Libro(models.Model):
+    
+    _name = "biblioteca.libro"
+    _description = "Libro"
