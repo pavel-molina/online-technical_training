b1e6169735168e35a81220f4fff0db84c967bf1e
pavel-molina <pavel.molina@patronato.unam.mx>
2021-05-01 23:01:08 +0000

    [ADD] Modelo Tarea

---
+++ tienda_cooperativa/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Tienda Cooperativa',
+    
+    'summary': """Organización de la Tienda Cooperativa""",
+    
+    'description': """
+          Organizará el trabajo de los voluntarios en la tienda cooperativa
+    """,
+    
+    'author': 'Pavel',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [   
+       
+    ],
+    
+    'demo':[
+        
+        
+    ],
+    
+}
---
+++ tienda_cooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Tarea(models.Model):
+    
+    _name = "tienda.tarea"
+    _description = "Tarea"
