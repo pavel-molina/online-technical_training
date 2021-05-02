09b1217be33087256aac8006c0d553804880f338
pavel-molina <pavel.molina@patronato.unam.mx>
2021-05-02 00:26:00 +0000

    [ADD] Fields

--- admin_biblioteca/models/.ipynb_checkpoints/libro-checkpoint.py
+++ admin_biblioteca/models/.ipynb_checkpoints/libro-checkpoint.py
@@ -6,3 +6,11 @@ class Libro(models.Model):
     
     _name = "biblioteca.libro"
     _description = "Libro"
+    
+    titulo = fields.Char(string="Titulo", required=True)
+    autor = fields.Char(string="Autor", required=True)
+    editorial = fields.Char(string="Editorial", required=True)
+    anio_edicion = fields.Integer(string="Año de Edición", required=True)
+    isbn = fields.Char(string="ISBN", required=True)
+    genero = fields.Char(string="Genero")
+    numero_paginas = fields.Integer(string="Número de Páginas")
