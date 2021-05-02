d63eb2a83b628520b0259a6c27f2b846e2cf1626
pavel-molina <pavel.molina@patronato.unam.mx>
2021-05-02 00:26:21 +0000

    [ADD] Fields

--- tienda_cooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
+++ tienda_cooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
@@ -6,3 +6,14 @@ class Tarea(models.Model):
     
     _name = "tienda.tarea"
     _description = "Tarea"
+    
+    nombre_tarea = fields.Char(string="Nombre de la Tarea", required=True)
+    descripcion = fields.Text(string='Descripcion', required=True)
+    tipo_tarea = fields.Char(string="Tipo de Tarea", required=True)
+    # now=datetime.strftime(fields.Datetime.context_timestamp(self, datetime.now()), "%Y-%m-%d %H:%M:%S")
+    hora_inicio = fields.DateTime(string='Hora Inicio', default=fields.DateTime.today)
+    hora_fin = fields.DateTime(string='Hora Fin', default=fields.DateTime.today)
+    
+    repite = fields.Boolean(string='Repite')
+    frecuencia = fields.Integer(string='Frecuencia')
+    
