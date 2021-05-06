e15675ef861145b30fe149792c5f8f6b15f041b4
pavel-molina <pavel.molina@patronato.unam.mx>
2021-05-06 04:45:06 +0000

    [Modify] Views

--- tienda_cooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
+++ tienda_cooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
@@ -11,8 +11,11 @@ class Tarea(models.Model):
     descripcion = fields.Text(string='Descripcion', required=True)
     tipo_tarea = fields.Char(string="Tipo de Tarea", required=True)
     # now=datetime.strftime(fields.Datetime.context_timestamp(self, datetime.now()), "%Y-%m-%d %H:%M:%S")
-    hora_inicio = fields.Datetime(string='Hora Inicio', default=fields.Datetime.today)
-    hora_fin = fields.Datetime(string='Hora Fin', default=fields.Datetime.today)
+    #hora_inicio = fields.Datetime(string='Hora Inicio', default=fields.Datetime.today)
+    #hora_fin = fields.Datetime(string='Hora Fin', default=fields.Datetime.today)
+    
+    hora_inicio = fields.Float(string='Hora Inicio', compute="_compute_time")
+    hora_fin =  fields.Float(string='Hora Fin', compute="_compute_time")
     
     repite = fields.Boolean(string='Repite')
     frecuencia = fields.Integer(string='Frecuencia')
