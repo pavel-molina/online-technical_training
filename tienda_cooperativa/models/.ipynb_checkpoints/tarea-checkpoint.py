ed9b578ee5881732b07d26c716510f18872c406b
pavel-molina <pavel.molina@patronato.unam.mx>
2021-05-11 00:43:45 +0000

    [ADD] Advanced views

--- tienda_cooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
+++ tienda_cooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
@@ -11,18 +11,21 @@ class Tarea(models.Model):
     descripcion = fields.Text(string='Descripcion', required=True)
     tipo_tarea = fields.Char(string="Tipo de Tarea", required=True)
     # now=datetime.strftime(fields.Datetime.context_timestamp(self, datetime.now()), "%Y-%m-%d %H:%M:%S")
-    #hora_inicio = fields.Datetime(string='Hora Inicio', default=fields.Datetime.today)
-    #hora_fin = fields.Datetime(string='Hora Fin', default=fields.Datetime.today)
+    hora_inicio = fields.Datetime(string='Hora Inicio', default=fields.Datetime.today)
+    hora_fin = fields.Datetime(string='Hora Fin', default=fields.Datetime.today)
     #hora_inicio = fields.Float(string='Time', compute="_compute_time")
     
-    hora_inicio = fields.Float(string='Hora Inicio')
-    hora_fin =  fields.Float(string='Hora Fin')
+    #hora_inicio = fields.Float(string='Hora Inicio')
+    #hora_fin =  fields.Float(string='Hora Fin')
+                
+                      
     
     repite = fields.Boolean(string='Repite')
     frecuencia = fields.Integer(string='Frecuencia')
     
     estado = fields.Char(string="Estado", default='Borrador')
-    lider = fields.Char(string="Lider")
+    #lider = fields.Char(string="Lider")
+    lider = fields.Many2one(comodel_name='res.partner', string='Lider')
     
     
     voluntarios_ids = fields.Many2many(comodel_name='res.partner', string='Voluntarios')
@@ -32,6 +35,5 @@ class Tarea(models.Model):
     def _onchange_total_price(self):
         if len(self.lider) > 0:
             self.estado = "Listo"
-            
             
     
